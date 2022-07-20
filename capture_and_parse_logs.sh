a61a4d48aace3a16a3d2e8c52c4494a447e3fc88
Gabriel Lando <gabriellando@hotmail.com>
2022-07-20 14:25:05 -0300

    Move Logs Parser to a different repository

--- capture_and_parse_logs.sh
+++ capture_and_parse_logs.sh
@@ -23,8 +23,8 @@ docker logs my5grantester > my5grantester_logs.txt
 
 # Download scripts to parse logs
 echo "Downloading scripts..."
-wget https://raw.githubusercontent.com/gabriel-lando/my5G-RANTester-Scripts/main/LogsParser/parse_tester_logs.js -O parse_tester_logs.js >/dev/null 2>&1
-wget https://raw.githubusercontent.com/gabriel-lando/my5G-RANTester-Scripts/main/LogsParser/Dockerfile -O Dockerfile >/dev/null 2>&1
+wget https://raw.githubusercontent.com/gabriel-lando/my5G-RANTester-Logs-Parser/main/index.js -O parse_tester_logs.js >/dev/null 2>&1
+wget https://raw.githubusercontent.com/gabriel-lando/my5G-RANTester-Logs-Parser/main/Dockerfile -O Dockerfile >/dev/null 2>&1
 
 # Parse logs
 echo "Parsing logs..."
