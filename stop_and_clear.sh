e6d084b0f9e77a1083f4a096cd0e1da3c4d81449
Lando, Gabriel <gabriellando@hotmail.com>
2022-07-23 15:52:14 -0300

    Update stop_and_clear.sh

--- stop_and_clear.sh
+++ stop_and_clear.sh
@@ -16,8 +16,20 @@ if [ -d "$METRICS_COLLECTOR_DIR" ]; then
     rm -rf /data/influxdb
 fi
 
-# Stop my5G-RANTester container and clear data
+# Stop free5GC my5G-RANTester container and clear data
 TESTER_DIR="free5gc-my5G-RANTester-docker/"
+if [ -d "$TESTER_DIR" ]; then
+    cd $TESTER_DIR
+    docker compose down --rmi all -v --remove-orphans
+
+    cd $WORK_DIR
+
+    # Remove git directory
+    rm -rf $TESTER_DIR
+fi
+
+# Stop Open5GS my5G-RANTester container and clear data
+TESTER_DIR="open5gs-my5G-RANTester-docker/"
 if [ -d "$TESTER_DIR" ]; then
     cd $TESTER_DIR
     docker compose down --rmi all -v --remove-orphans
@@ -40,8 +52,32 @@ if [ -d "$FREE5GC_CORE_DIR" ]; then
     rm -rf $FREE5GC_CORE_DIR
 fi
 
+# Stop Open5GS containers and clear data
+OPEN5GS_CORE_DIR="docker_open5gs/"
+if [ -d "$OPEN5GS_CORE_DIR" ]; then
+    cd $OPEN5GS_CORE_DIR
+    docker compose down --rmi all -v --remove-orphans
+
+    cd $WORK_DIR
+
+    # Remove git directory
+    rm -rf $OPEN5GS_CORE_DIR
+fi
+
 # Clear free5GC database filler data
 DATABASE_FILLER_DIR="my5G-RANTester-free5GC-Database-Filler/"
+if [ -d "$DATABASE_FILLER_DIR" ]; then
+    cd $DATABASE_FILLER_DIR
+    docker compose down --rmi all -v --remove-orphans
+
+    cd $WORK_DIR
+
+    # Remove git directory
+    rm -rf $DATABASE_FILLER_DIR
+fi
+
+# Clear Open5GS database filler data
+DATABASE_FILLER_DIR="my5G-RANTester-Open5GS-Database-Filler/"
 if [ -d "$DATABASE_FILLER_DIR" ]; then
     cd $DATABASE_FILLER_DIR
     docker compose down --rmi all -v --remove-orphans
