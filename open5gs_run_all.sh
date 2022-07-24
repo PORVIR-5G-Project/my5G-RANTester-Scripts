2ab934a4de4ab96a73100cb421233726741278c5
Lando, Gabriel <gabriellando@hotmail.com>
2022-07-23 23:26:11 -0300

    Fix typo

--- open5gs_run_all.sh
+++ open5gs_run_all.sh
@@ -125,7 +125,7 @@ VERSION_EXPECTED=5.4
 CURRENT_VERSION=$(uname -r | cut -c1-3)
 print "Checking Kernel version..."
 if (( $(echo "$CURRENT_VERSION == $VERSION_EXPECTED" |bc -l) )); then
-    print_ok "->Kernel version $CURRENT_VERSION OK."
+    print_ok "-> Kernel version $CURRENT_VERSION OK."
 else
     print_err "-> You are NOT running the recommended kernel version. Please install version 5.4.90-generic."
     user_input "Do you want to continue (NOT recommended)? [y/N] "
