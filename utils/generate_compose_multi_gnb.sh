0ba4153a25afca5e4e50fdcb1c6c07be9a304e08
Lando, Gabriel <gabriellando@hotmail.com>
2022-08-07 02:57:04 -0300

    Update config path

--- utils/generate_compose_multi_gnb.sh
+++ utils/generate_compose_multi_gnb.sh
@@ -6,7 +6,8 @@ WORK_DIR=$(pwd)
 ### Default value of CLI parameters
 IN_COMPOSE_NAME=docker-compose.yaml
 OUT_COMPOSE_NAME=docker-multi.yaml
-SETTINGS_FILE=config/tester.yaml
+SETTINGS_PATH=config
+SETTINGS_FILE=tester.yaml
 NUM_GNBs=1
 NUM_UEs=1
 
@@ -22,18 +23,20 @@ show_help(){
     echo "  -g int  Set the number of gNodeB to test. (Defaut: $NUM_GNBs)"
     echo "  -i str  Set the name of the input Docker Compose file. (Defaut: $IN_COMPOSE_NAME)"
     echo "  -o str  Set the name of the output Docker Compose file. (Defaut: $OUT_COMPOSE_NAME)"
-    echo "  -s str  Settings file path. (Defaut: $SETTINGS_FILE)"
+    echo "  -p str  Settings file path. (Defaut: ./$SETTINGS_PATH/)"
+    echo "  -s str  Settings file name. (Defaut: $SETTINGS_FILE)"
     echo "  -u int  Set the number of UEs to test. (Defaut: $NUM_UEs)"
     echo ""
 }
 
 # Parse CLI parameters
-while getopts ':g:i:o:s:u:h' 'OPTKEY'; do
+while getopts ':g:i:o:p:s:u:h' 'OPTKEY'; do
     case ${OPTKEY} in
         h) show_help; exit 0 ;;
         g) NUM_GNBs=$OPTARG ;;
         i) IN_COMPOSE_NAME=$OPTARG ;;
         o) OUT_COMPOSE_NAME=$OPTARG ;;
+        p) SETTINGS_PATH=$OPTARG ;;
         s) SETTINGS_FILE=$OPTARG ;;
         u) NUM_UEs=$OPTARG ;;
     esac
@@ -50,6 +53,7 @@ touch $WORK_DIR/$OUT_COMPOSE_NAME
 echo TESTER_PATH=$WORK_DIR > .env
 echo NUM_UE=$NUM_UEs >> .env
 echo NUM_GNB=$NUM_GNBs >> .env
+echo CONFIG_PATH=$SETTINGS_PATH >> .env
 echo CONFIG_FILE=$SETTINGS_FILE >> .env
 echo IN_COMPOSE=$IN_COMPOSE_NAME >> .env
 echo OUT_COMPOSE=$OUT_COMPOSE_NAME >> .env
