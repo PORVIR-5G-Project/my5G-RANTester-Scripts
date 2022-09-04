47725db089cee13ab3233735b0f2ebb5458a02ba
Gabriel Lando <gabriellando@hotmail.com>
2022-09-04 00:09:04 -0300

    Add multiple executions for experiment 2

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Run connectivity tests"
-
 for c in 2 3; do
     echo "Run core $c tests"
     for w in 500 400 300 200 100; do
@@ -13,10 +12,10 @@ for c in 2 3; do
             sleep $((2*60))
 
             echo "Collecting experiment $i (w=$w) data"
-            bash <(curl -s https://raw.githubusercontent.com/gabriel-lando/my5G-RANTester-Scripts/throughput-test/capture_and_parse_logs.sh) my5grantester-logs-$c-$i-$w.csv
+            bash <(curl -s https://raw.githubusercontent.com/gabriel-lando/my5G-RANTester-Scripts/throughput-test/capture_and_parse_logs.sh) my5grantester-logs-$c-$w-$i.csv
 
             echo "Collecting experiment $i data from influxdb"
-            docker exec influxdb sh -c "influx query 'from(bucket:\"database\") |> range(start:-5m)' --raw" > my5grantester-logs-influxdb-$c-$i-$w.csv
+            docker exec influxdb sh -c "influx query 'from(bucket:\"database\") |> range(start:-5m)' --raw" > my5grantester-logs-influxdb-$c-$w-$i.csv
 
             echo "Clear experiment $i (w=$w) environment"
             bash stop_only.sh >/dev/null 2>&1
@@ -26,33 +25,39 @@ for c in 2 3; do
     done
 done
 
+echo "Clean environment"
+bash <(curl -s https://raw.githubusercontent.com/gabriel-lando/my5G-RANTester-Scripts/throughput-test/stop_and_clear.sh)
+docker image prune -a -f >/dev/null 2>&1
+docker volume prune -f >/dev/null 2>&1
 
 echo "Run throughtput tests"
+# Run 15 times all throughput tests
+for e in $(seq 1 16); do
+    for c in 1 3; do
+        echo "Run core $c tests (exec $e)"
+        for i in 1 2 4 6 8 10; do
+            echo "Running experiment $i"
+            bash <(curl -s https://raw.githubusercontent.com/gabriel-lando/my5G-RANTester-Scripts/throughput-test/run.sh) -c $c -e 2 -g $i
 
-for c in 1 3; do
-    echo "Run core $c tests"
-    for i in 1 2 4 6 8 10; do
-        echo "Running experiment $i"
-        bash <(curl -s https://raw.githubusercontent.com/gabriel-lando/my5G-RANTester-Scripts/throughput-test/run.sh) -c $c -e 2 -g $i
+            echo "Waiting connections for experiment $i"
+            sleep $((1*60))
 
-        echo "Waiting connections for experiment $i"
-        sleep $((1*60))
+            echo "Starting experiment $i"
+            for j in $(seq 0 $(($i - 1))); do
+                IP=$(docker exec -ti my5grantester$j sh -c "ip -4 addr show uetun1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'")
+                docker exec my5grantester$j sh -c "iperf -c iperf --bind $IP -t 60 -i 1 -y C" > my5grantester-iperf-$e-$c-$i-$j.csv &
+            done
 
-        echo "Starting experiment $i"
-        for j in $(seq 0 $(($i - 1))); do
-            IP=$(docker exec -ti my5grantester$j sh -c "ip -4 addr show uetun1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'")
-            docker exec my5grantester$j sh -c "iperf -c iperf --bind $IP -t 60 -i 1 -y C" > my5grantester-iperf-$c-$i-$j.csv &
+            echo "Waiting for experiment $i to finish"
+            sleep $((80))
+
+            echo "Collecting experiment $i data from influxdb"
+            docker exec influxdb sh -c "influx query 'from(bucket:\"database\") |> range(start:-5m)' --raw" > my5grantester-iperf-influxdb-$e-$c-$i.csv
+
+            echo "Clear experiment $i environment"
+            bash stop_only.sh >/dev/null 2>&1
+            docker image prune --filter="dangling=true" -f >/dev/null 2>&1
+            docker volume prune -f >/dev/null 2>&1
         done
-
-        echo "Waiting for experiment $i to finish"
-        sleep $((80))
-
-        echo "Collecting experiment $i data from influxdb"
-        docker exec influxdb sh -c "influx query 'from(bucket:\"database\") |> range(start:-5m)' --raw" > my5grantester-iperf-influxdb-$c-$i.csv
-
-        echo "Clear experiment $i environment"
-        bash stop_only.sh >/dev/null 2>&1
-        docker image prune --filter="dangling=true" -f >/dev/null 2>&1
-        docker volume prune -f >/dev/null 2>&1
     done
 done
