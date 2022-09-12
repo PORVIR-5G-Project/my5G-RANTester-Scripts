e44aae65145e4b3c1b9d0f210312ce3ca0d1b79a
Lando, Gabriel <gabriellando@hotmail.com>
2022-09-12 11:31:16 -0300

    Run 10 times each connectivity experiment

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,28 +2,30 @@
 # Run: screen -L -Logfile output.txt ./run_experiments.sh
 
 echo "Run connectivity tests"
-for c in 2 3; do
-    echo "Run core $c tests"
-    for w in 500 400 300 200 100; do
-        for i in 1 3 5 7 9 11; do
-            echo "Running experiment $i (w=$w)"
-            bash <(curl -s https://raw.githubusercontent.com/gabriel-lando/my5G-RANTester-Scripts/throughput-test/run.sh) -c $c -e 1 -g $i -u $((100*$i)) -t 60 -w $w -v
+for e in $(seq 1 10); do
+    for c in 2 3; do
+        echo "Run core $c tests (exec $e)"
+        for w in 500 400 300 200 100; do
+            for i in 1 3 5 7 9 11; do
+                echo "Running experiment $i (w=$w)"
+                bash <(curl -s https://raw.githubusercontent.com/gabriel-lando/my5G-RANTester-Scripts/throughput-test/run.sh) -c $c -e 1 -g $i -u $((100*$i)) -t 60 -w $w -v
 
-            echo "Waiting for experiment $i (w=$w) to finish"
-            sleep $((2*60))
+                echo "Waiting for experiment $i (w=$w) to finish"
+                sleep $((2*60))
 
-            echo "Collecting experiment $i (w=$w) data"
-            bash <(curl -s https://raw.githubusercontent.com/gabriel-lando/my5G-RANTester-Scripts/throughput-test/capture_and_parse_logs.sh) my5grantester-logs-$c-$w-$i.csv
+                echo "Collecting experiment $i (w=$w) data"
+                bash <(curl -s https://raw.githubusercontent.com/gabriel-lando/my5G-RANTester-Scripts/throughput-test/capture_and_parse_logs.sh) my5grantester-logs-$e-$c-$w-$i.csv
 
-            echo "Collecting experiment $i data from influxdb"
-            docker exec influxdb sh -c "influx query 'from(bucket:\"database\") |> range(start:-5m)' --raw" > my5grantester-logs-influxdb-$c-$w-$i.csv
+                echo "Collecting experiment $i data from influxdb"
+                docker exec influxdb sh -c "influx query 'from(bucket:\"database\") |> range(start:-5m)' --raw" > my5grantester-logs-influxdb-$e-$c-$w-$i.csv
 
-            echo "Clear experiment $i (w=$w) environment"
-            bash stop_only.sh
-            docker image prune --filter="dangling=true" -f
-            docker volume prune -f
+                echo "Clear experiment $i (w=$w) environment"
+                bash stop_only.sh
+                docker image prune --filter="dangling=true" -f
+                docker volume prune -f
 
-            sleep 15
+                sleep 15
+            done
         done
     done
 done
