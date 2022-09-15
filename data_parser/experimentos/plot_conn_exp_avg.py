9375980939c60b878629f419a7b6f2237529d7de
Lando, Gabriel <gabriellando@hotmail.com>
2022-09-15 03:40:31 -0300

    Improve script to generate exp1 charts with average data to be used on TCC

--- data_parser/experimentos/plot_conn_exp_avg.py
+++ data_parser/experimentos/plot_conn_exp_avg.py
@@ -6,13 +6,13 @@ import numpy as np
 INITIAL_UE = 43
 UE_PER_GNB = 100
 
-vm_configs = [ "TG2-8C-8GB", "TG2-6C-8GB", "TG2-4C-4GB" ]
+vm_configs = [ "TG2-12C-8GB_avg" ] #[ "TG2-8C-8GB", "TG2-6C-8GB", "TG2-4C-4GB" ]
 cores_name = [ 'free5GC', 'Open5GS' ]
 cores = [ 2, 3 ]
 execs = [ 500, 400, 300, 200, 100 ]     # Delays between connections in ms
 experiments = [ 1, 3, 5, 7, 9, 11 ]     # Experiments 1 to 11, step 2
 
-MAX_AXIS_X = 2
+MAX_AXIS_X = 5
 MAX_AXIS_Y = 2
 
 plt.style.use('seaborn-whitegrid')
@@ -20,16 +20,17 @@ plt.style.use('seaborn-whitegrid')
 for vm in vm_configs:
     base_filename = vm + "\\" + "my5grantester-logs-{}-{}-{}.csv"
 
-    for core_idx, core in enumerate(cores):
 
+    figure, axis = plt.subplots(MAX_AXIS_X, MAX_AXIS_Y)
+    figure.canvas.manager.set_window_title(vm)   
+
+    for core_idx, core in enumerate(cores):
         axis_x = 0
-        axis_y = 0
-        figure, axis = plt.subplots(MAX_AXIS_X, MAX_AXIS_Y)
-        figure.canvas.manager.set_window_title(cores_name[core_idx] + " - " + vm)
 
         for exe in execs:
+            all_data = np.array([])
+
             for exp in experiments:
-                time_base = 0
                 timestamp = np.array([])
                 dataplaneready = np.array([])
 
@@ -40,22 +41,25 @@ for vm in vm_configs:
                 dataplane_pvar = 0
                 dataplane_pstdev = 0
 
+                curr_ax = axis[axis_x][core_idx]
+
                 with open(base_filename.format(core, exe, exp), newline='') as csvfile:
                     reader = csv.DictReader(csvfile)
 
                     for row in reader:
-                        if time_base == 0:
-                            time_base = int(row['timestamp'])
                         
                         try:
-                            dataplaneready = np.append(dataplaneready, float(row['DataPlaneReady']))
-                            timestamp = np.append(timestamp, (int(row['timestamp']) - time_base) / 1000000000)
+                            curr_dpr = float(row['DataPlaneReady_Avg'])
+                            curr_ts = float(row['timestamp'])
+
+                            dataplaneready = np.append(dataplaneready, curr_dpr)
+                            timestamp = np.append(timestamp, curr_ts)
                             
                             try:
                                 index_x = (int(row['gnbid']) - INITIAL_UE) % UE_PER_GNB
 
-                                avg_per_brust[index_x] = np.append(avg_per_brust[index_x], float(row['DataPlaneReady']))
-                                avg_ts_per_brust[index_x] = np.append(avg_ts_per_brust[index_x], (int(row['timestamp']) - time_base) / 1000000000)
+                                avg_per_brust[index_x] = np.append(avg_per_brust[index_x], curr_dpr)
+                                avg_ts_per_brust[index_x] = np.append(avg_ts_per_brust[index_x], curr_ts)
 
                             except Exception as e:
                                 print("Error: {}".format(e))
@@ -72,12 +76,14 @@ for vm in vm_configs:
                             timestamp_to_plot = np.append(timestamp_to_plot, statistics.mean(avg_ts_per_brust[i]))
 
                     # Get color
-                    color = next(axis[axis_x, axis_y]._get_lines.prop_cycler)['color']
+                    color = next(curr_ax._get_lines.prop_cycler)['color']
 
                     # Plot brust average
-                    axis[axis_x, axis_y].scatter(timestamp_to_plot, avg_to_plot, label = "#gNB {}".format(exp), s=5, color=color)
+                    curr_ax.scatter(timestamp_to_plot, avg_to_plot, label = "#gNB {}".format(exp), s=5, color=color)
 
                     # Calculate the average and standard deviation
+                    all_data = np.append(all_data, avg_to_plot)
+
                     try:
                         dataplane_avg = statistics.mean(dataplaneready)
                         dataplane_pvar = statistics.pvariance(dataplaneready)
@@ -86,20 +92,30 @@ for vm in vm_configs:
                         continue
 
                     # Plot the average
+                    print ("Core {}: {}ms - Exp {:2d}: {:.2f}ms".format(cores_name[core_idx], exe, exp, dataplane_avg))
+
                     dataplane_avg_arr = np.repeat(dataplane_avg, dataplaneready.size)
-                    axis[axis_x, axis_y].plot(timestamp, dataplane_avg_arr, color=color)
+                    curr_ax.plot(timestamp, dataplane_avg_arr, color=color)
 
-            axis[axis_x, axis_y].set_title("{} (Delay {})".format(cores_name[core_idx], exe))
-            axis_y += 1
-            if axis_y == MAX_AXIS_Y:
-                axis_y = 0
-                axis_x += 1
+            # Plot the global average
+            print ("Core {}: {}ms - Global: {:.2f}ms - Max: {:.2f}ms - Median: {:.2f}ms".format(cores_name[core_idx], exe, statistics.mean(all_data), max(all_data), statistics.median(all_data)))
 
-            if (axis_x == MAX_AXIS_X):
-                break
+            curr_ax.set_title(str(exe) + " ms", fontsize=10)
+            axis_x += 1
 
-        #figure.legend()        
-        plt.show(block=False)
+    figure.text(.55, 0.98, 'Tempo entre cada conexão (ms)', ha='center', fontsize=12)
+    figure.text(.33, 0.96, cores_name[0], ha='center', fontsize=12)
+    figure.text(.77, 0.96, cores_name[1], ha='center', fontsize=12)
+
+    figure.text(0.5, 0.04, "Tempo do experimento (s)", ha='center', fontsize=12)
+    figure.text(0.04, 0.5, "Tempo de conexão do UE (ms)", va='center', rotation='vertical', fontsize=12)
+    figure.tight_layout(rect=[0.15, 0.1, 0.95, 0.92])
+
+    plt.subplots_adjust(left=0.15, bottom=0.1, right=0.95, top=0.92, wspace=0.25, hspace=0.65)
+
+        #figure.legend()  
+        #figure.tight_layout()      
+    plt.show(block=False)
         #plt.savefig(cores_name[core_idx] + " - " + vm + ".svg", format="svg")
 
 input("Press Enter to continue...")
