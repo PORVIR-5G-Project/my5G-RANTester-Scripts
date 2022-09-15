90eb49877893632fac3688da00202e5679d75864
Lando, Gabriel <gabriellando@hotmail.com>
2022-09-15 02:00:57 -0300

    Improve script to calculate averages for exp 1 and improve charts for connectivity error

--- data_parser/experimentos/parse_exp_1_avg.py
+++ data_parser/experimentos/parse_exp_1_avg.py
@@ -8,14 +8,18 @@ UE_PER_GNB = 100
 vm_configs = [ "TG2-12C-8GB" ]
 cores_name = [ 'free5GC', 'Open5GS' ]
 cores = [ 2, 3 ]
-execs = list(range(1, 10))              # Executions 1 to 9
+execs = list(range(1, 11))              # Executions 1 to 10
 delays = [ 500, 400, 300, 200, 100 ]    # Delays between connections in ms
 experiments = [ 1, 3, 5, 7, 9, 11 ]     # Experiments 1 to 11, step 2
 
 final_results_headers = [ "gnbid", "timestamp", "DataPlaneReady_Avg", "DataPlaneReady_PVar", "DataPlaneReady_PStDev" ]
+conn_error_headers = [ "core_idx", "core", "core_name", "delay_connection", "num_gnb", "num_ue", "num_ue_fail_sum", "num_ue_fail", "ue_fail_percent", "ue_fail_pvar", "ue_fail_pstdev" ]
 
 for vm in vm_configs:
     base_filename = vm + "\\" + "my5grantester-logs-{}-{}-{}-{}.csv"
+
+    err_out_file = open(vm + "_logs.csv", "w")
+    err_out_file.write(','.join(conn_error_headers) + '\n')	# Write headers
 
     for core_idx, core in enumerate(cores):
         for delay in delays:
@@ -26,8 +30,12 @@ for vm in vm_configs:
                 out_filename = vm + "_avg" + "\\" + "my5grantester-logs-{}-{}-{}.csv".format(core, delay, exp)
                 out_file = open(out_filename, "w")
                 out_file.write(','.join(final_results_headers) + '\n')	# Write headers
+
+                fails_avg = np.array([])
+
                 for exe in execs:
                     time_base = 0
+                    conn_counter = 0
 
                     try:
                         input_filename = base_filename.format(exe, core, delay, exp)
@@ -46,25 +54,47 @@ for vm in vm_configs:
                                     all_timestamps[gnbid - INITIAL_UE] = np.append(all_timestamps[gnbid - INITIAL_UE], timestamp)
                                     all_dpready[gnbid - INITIAL_UE] = np.append(all_dpready[gnbid - INITIAL_UE], dataplaneready)
 
-                                except:
+                                    conn_counter += 1
+                                except Exception as e:
+                                    # print("Error parsing file: " + e.__str__() + " " + input_filename)
                                     continue
                     except Exception as e:
                         print("Error reading file: " + e.__str__() + " " + input_filename)
-                        continue
+
+                    fails_qty = exp * UE_PER_GNB - conn_counter
+                    fails_avg = np.append(fails_avg, fails_qty)
 
                 for gnbid in range(INITIAL_UE, INITIAL_UE + (UE_PER_GNB * exp)):
-                    # out_file.write(str(gnbid) + ',' + str(statistics.mean(all_timestamps[gnbid - INITIAL_UE])) + ',' + str(statistics.mean(all_dpready[gnbid - INITIAL_UE])) + ',' + str(statistics.pvariance(all_dpready[gnbid - INITIAL_UE])) + ',' + str(statistics.pstdev(all_dpready[gnbid - INITIAL_UE])) + '\n')
-
                     timestamp_avg = 0
-                    try:
+                    dataplaneready_avg = 0
+                    dataplaneready_pvar = 0
+                    dataplaneready_pstdev = 0
+                    if all_dpready[gnbid - INITIAL_UE].size > 0:
                         timestamp_avg = statistics.mean(all_timestamps[gnbid - INITIAL_UE])
                         dataplaneready_avg = statistics.mean(all_dpready[gnbid - INITIAL_UE])
                         dataplaneready_pvar = statistics.pvariance(all_dpready[gnbid - INITIAL_UE])
                         dataplaneready_pstdev = statistics.pstdev(all_dpready[gnbid - INITIAL_UE])
                         out_file.write(str(gnbid) + ',' + str(timestamp_avg) + ',' + str(dataplaneready_avg) + ',' + str(dataplaneready_pvar) + ',' + str(dataplaneready_pstdev) + '\n')
-                    except Exception as e:
-                        print("Error writing file: " + e.__str__() + " " + out_filename)
+                    else:
+                        print("Error: Empty all_timestamps[" + str(gnbid) + "]: " + out_filename)
                         out_file.write(str(gnbid) + ',' + str(timestamp_avg) + ',' + ',' + ',' + '\n')
-
                 out_file.close()
+                
+                num_ue_fail_sum = 0
+                num_ue_fail_avg = 0
+                ue_fail_percent_avg = 0
+                ue_fail_pvar = 0
+                ue_fail_pstdev = 0
+                if (fails_avg.size > 0):
+                    num_ue_fail_sum = fails_avg.sum()
+                    num_ue_fail_avg = statistics.mean(fails_avg)
+                    ue_fail_percent_avg = num_ue_fail_avg * 100 / (exp * UE_PER_GNB)
+                    ue_fail_pvar = statistics.pvariance(fails_avg)
+                    ue_fail_pstdev = statistics.pstdev(fails_avg)
+
+                results = [ core_idx, core, cores_name[core_idx], delay, exp, exp * UE_PER_GNB, num_ue_fail_sum, num_ue_fail_avg, ue_fail_percent_avg, ue_fail_pvar, ue_fail_pstdev ]
+                err_out_file.write(','.join(map(str, results)) + '\n')
+    
+    err_out_file.close()
+                
 
--- data_parser/experimentos/plot_conn_err.py
+++ data_parser/experimentos/plot_conn_err.py
@@ -1,17 +1,25 @@
 import csv
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as path_effects
 import numpy as np
 
-MAX_AXIS_X = 2
-MAX_AXIS_Y = 2
+MAX_AXIS_X = 5
+MAX_AXIS_Y = 1
 
-vm_configs = [ "TG2-12C-8GB_avg" ] #[ "TG2-8C-8GB", "TG2-6C-8GB", "TG2-4C-4GB" ]
+COLOR_FREE5GC_MAIN = "orange"
+COLOR_FREE5GC_LINE = "tab:orange"
+
+COLOR_OPEN5GS_MAIN = "cornflowerblue"
+COLOR_OPEN5GS_LINE = "royalblue"
+
+vm_configs = [ "TG2-12C-8GB" ] #[ "TG2-8C-8GB", "TG2-6C-8GB", "TG2-4C-4GB" ]
 cores_name = [ 'free5GC', 'Open5GS' ]
 execs = [ 500, 400, 300, 200, 100 ]     # Delays between connections in ms
 cores = [ 2, 3 ]                        # free5GC and Open5GS
 experiments = [ 1, 3, 5, 7, 9, 11 ]     # Experiments 1 to 11, step 2
 
 core_res = np.zeros((len(execs), len(experiments), len(cores)), dtype=float)
+core_stats = np.zeros((len(execs), len(experiments), len(cores)), dtype=float)
 
 for vm in vm_configs:
     base_filename = vm + "_logs.csv"
@@ -28,30 +36,49 @@ for vm in vm_configs:
             delay_connection = int(row['delay_connection'])
             core = int(row['core'])
             num_gnb = int(row['num_gnb'])
+            ue_fail_sum = float(row['num_ue_fail_sum'])
             ue_fail_percent = float(row['ue_fail_percent'])
+            ue_fail_avg = float(row['num_ue_fail'])
+            ue_fail_pvar = float(row['ue_fail_pvar'])
+            ue_fail_stdev = float(row['ue_fail_pstdev'])
 
             # core_res = np.append(core_res, [delay_connection, core, num_gnb, ue_fail_percent])
             idx_exe = execs.index(delay_connection)
             idx_core = cores.index(core)
             idx_exp = experiments.index(num_gnb)
 
-            core_res[idx_exe][idx_exp][idx_core] = ue_fail_percent
+            core_res[idx_exe][idx_exp][idx_core] = ue_fail_avg
+            core_stats[idx_exe][idx_exp][idx_core] = ue_fail_stdev
 
     for idx_exe, exe in enumerate(execs):
         width = 0.8
         x = np.array(experiments)
 
-        rects1 = axis[axis_x][axis_y].bar(x - width/2, core_res[idx_exe][:,0], width, label=cores_name[0])
-        rects2 = axis[axis_x][axis_y].bar(x + width/2, core_res[idx_exe][:,1], width, label=cores_name[1])
-        
-        axis[axis_x][axis_y].set_title(str(exe) + " ms")
-        axis[axis_x][axis_y].set_xlabel("Number of gNBs")
-        axis[axis_x][axis_y].set_ylabel("UEs failed (%)")
-        axis[axis_x][axis_y].set_xticks(experiments)
-        axis[axis_x][axis_y].legend()
+        curr_ax = None
+        if (MAX_AXIS_Y == 1):
+            curr_ax = axis[axis_x]
+        elif (MAX_AXIS_X == 1):
+            curr_ax = axis[axis_y]
+        else:
+            curr_ax = axis[axis_y][axis_x]
 
-        # axis[axis_x][axis_y].bar_label(rects1, padding=3)
-        # axis[axis_x][axis_y].bar_label(rects2, padding=3)
+        rects1 = curr_ax.bar(x - width/2, core_res[idx_exe][:,0], width, label=cores_name[0], color=COLOR_FREE5GC_MAIN)
+        rects2 = curr_ax.bar(x + width/2, core_res[idx_exe][:,1], width, label=cores_name[1], color=COLOR_OPEN5GS_MAIN)
+
+        rects1 = curr_ax.plot(x, core_stats[idx_exe][:,0], color=COLOR_FREE5GC_LINE) #, path_effects=[path_effects.SimpleLineShadow(), path_effects.Normal()])
+        rects2 = curr_ax.plot(x, core_stats[idx_exe][:,1], color=COLOR_OPEN5GS_LINE) #, path_effects=[path_effects.SimpleLineShadow(), path_effects.Normal()])
+
+        rects1 = curr_ax.scatter(x, core_stats[idx_exe][:,0], s=10, color=COLOR_FREE5GC_LINE)
+        rects2 = curr_ax.scatter(x, core_stats[idx_exe][:,1], s=10, color=COLOR_OPEN5GS_LINE)
+        
+        curr_ax.set_title(str(exe) + " ms")
+        # curr_ax.set_xlabel("Numero de gNBs")
+        # curr_ax.set_ylabel("Average UEs failed")
+        curr_ax.set_xticks(experiments)
+        curr_ax.legend()
+
+        # curr_ax.bar_label(rects1, padding=3)
+        # curr_ax.bar_label(rects2, padding=3)
 
         axis_y += 1
             
@@ -62,7 +89,17 @@ for vm in vm_configs:
         if (axis_x == MAX_AXIS_X):
             break
 
-    figure.tight_layout()
+
+    figure.suptitle('Tempo entre cada conexão (ms)')
+    figure.text(0.5, 0.04, "Numero de gNBs por núcleo", ha='center')
+    figure.text(0.04, 0.5, "Média e desvio padrão de falhas de conexão", va='center', rotation='vertical')
+    figure.tight_layout(rect=[0.15, 0.1, 0.95, 0.93])
+
+    plt.subplots_adjust(left=0.15, bottom=0.1, right=0.95, top=0.93, wspace=0.5, hspace=0.65)
+
+    # mng = plt.get_current_fig_manager()
+    # mng.resize(*mng.window.maxsize())
     plt.show(block=False)
+    # plt.savefig(vm + ".svg", format="svg")
 
 input("Press Enter to continue...")
