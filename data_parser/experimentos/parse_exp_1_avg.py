549788ab7f412994c0e929833ec86c3a16648646
Lando, Gabriel <gabriellando@hotmail.com>
2022-09-16 01:25:14 -0300

    Add mean for all other connection states

--- data_parser/experimentos/parse_exp_1_avg.py
+++ data_parser/experimentos/parse_exp_1_avg.py
@@ -12,7 +12,12 @@ execs = list(range(1, 11))              # Executions 1 to 10
 delays = [ 500, 400, 300, 200, 100 ]    # Delays between connections in ms
 experiments = [ 1, 3, 5, 7, 9, 11 ]     # Experiments 1 to 11, step 2
 
-final_results_headers = [ "gnbid", "timestamp", "DataPlaneReady_Avg", "DataPlaneReady_PVar", "DataPlaneReady_PStDev" ]
+final_results_headers = [ "gnbid", "timestamp",
+                        "DataPlaneReady_Avg", "DataPlaneReady_PVar", "DataPlaneReady_PStDev",
+                        "SM5G_PDU_SESSION_INACTIVE", "MM5G_NULL", "MM5G_DEREGISTERED",
+                        "MM5G_REGISTERED_INITIATED", "MM5G_REGISTERED",
+                        "SM5G_PDU_SESSION_ACTIVE_PENDING", "SM5G_PDU_SESSION_ACTIVE" ]
+
 conn_error_headers = [ "core_idx", "core", "core_name", "delay_connection", "num_gnb", "num_ue", "num_ue_fail_sum", "num_ue_fail", "ue_fail_percent", "ue_fail_pvar", "ue_fail_pstdev" ]
 
 for vm in vm_configs:
@@ -26,6 +31,14 @@ for vm in vm_configs:
             for exp in experiments:
                 all_timestamps = [np.array([])] * (UE_PER_GNB * exp)
                 all_dpready = [np.array([])] * (UE_PER_GNB * exp)
+
+                all_sess_inactive = [np.array([])] * (UE_PER_GNB * exp)
+                all_null = [np.array([])] * (UE_PER_GNB * exp)
+                all_dereg = [np.array([])] * (UE_PER_GNB * exp)
+                all_reg_init = [np.array([])] * (UE_PER_GNB * exp)
+                all_reg = [np.array([])] * (UE_PER_GNB * exp)
+                all_sess_active_pending = [np.array([])] * (UE_PER_GNB * exp)
+                all_sess_active = [np.array([])] * (UE_PER_GNB * exp)
                 
                 out_filename = vm + "_avg" + "\\" + "my5grantester-logs-{}-{}-{}.csv".format(core, delay, exp)
                 out_file = open(out_filename, "w")
@@ -51,8 +64,18 @@ for vm in vm_configs:
                                     timestamp = (int(row['timestamp']) - time_base) / 1000000000
                                     gnbid = int(row['gnbid'])
 
+
                                     all_timestamps[gnbid - INITIAL_UE] = np.append(all_timestamps[gnbid - INITIAL_UE], timestamp)
                                     all_dpready[gnbid - INITIAL_UE] = np.append(all_dpready[gnbid - INITIAL_UE], dataplaneready)
+
+                                    all_sess_inactive[gnbid - INITIAL_UE] = np.append(all_sess_inactive[gnbid - INITIAL_UE], float(row['SM5G_PDU_SESSION_INACTIVE']))
+                                    all_null[gnbid - INITIAL_UE] = np.append(all_null[gnbid - INITIAL_UE], float(row['MM5G_NULL']))
+                                    all_dereg[gnbid - INITIAL_UE] = np.append(all_dereg[gnbid - INITIAL_UE], float(row['MM5G_DEREGISTERED']))
+                                    all_reg_init[gnbid - INITIAL_UE] = np.append(all_reg_init[gnbid - INITIAL_UE], float(row['MM5G_REGISTERED_INITIATED']))
+                                    all_reg[gnbid - INITIAL_UE] = np.append(all_reg[gnbid - INITIAL_UE], float(row['MM5G_REGISTERED']))
+                                    all_sess_active_pending[gnbid - INITIAL_UE] = np.append(all_sess_active_pending[gnbid - INITIAL_UE], float(row['SM5G_PDU_SESSION_ACTIVE_PENDING']))
+                                    all_sess_active[gnbid - INITIAL_UE] = np.append(all_sess_active[gnbid - INITIAL_UE], float(row['SM5G_PDU_SESSION_ACTIVE']))
+
 
                                     conn_counter += 1
                                 except Exception as e:
@@ -74,10 +97,25 @@ for vm in vm_configs:
                         dataplaneready_avg = statistics.mean(all_dpready[gnbid - INITIAL_UE])
                         dataplaneready_pvar = statistics.pvariance(all_dpready[gnbid - INITIAL_UE])
                         dataplaneready_pstdev = statistics.pstdev(all_dpready[gnbid - INITIAL_UE])
-                        out_file.write(str(gnbid) + ',' + str(timestamp_avg) + ',' + str(dataplaneready_avg) + ',' + str(dataplaneready_pvar) + ',' + str(dataplaneready_pstdev) + '\n')
+
+                        sess_inactive_avg = statistics.mean(all_sess_inactive[gnbid - INITIAL_UE])
+                        null_avg = statistics.mean(all_null[gnbid - INITIAL_UE])
+                        dereg_avg = statistics.mean(all_dereg[gnbid - INITIAL_UE])
+                        reg_init_avg = statistics.mean(all_reg_init[gnbid - INITIAL_UE])
+                        reg_avg = statistics.mean(all_reg[gnbid - INITIAL_UE])
+                        sess_active_pending_avg = statistics.mean(all_sess_active_pending[gnbid - INITIAL_UE])
+                        sess_active_avg = statistics.mean(all_sess_active[gnbid - INITIAL_UE])
+
+                        res = [gnbid, timestamp_avg, dataplaneready_avg, dataplaneready_pvar, dataplaneready_pstdev,
+                            sess_inactive_avg, null_avg, dereg_avg, reg_init_avg, reg_avg, sess_active_pending_avg, sess_active_avg]
+
+                        out_file.write(','.join(map(str, res)) + '\n')
                     else:
                         print("Error: Empty all_timestamps[" + str(gnbid) + "]: " + out_filename)
-                        out_file.write(str(gnbid) + ',' + str(timestamp_avg) + ',' + ',' + ',' + '\n')
+
+                        res = [gnbid, timestamp_avg]
+                        out_file.write(','.join(map(str, res)) + ',' * 10 + '\n')
+
                 out_file.close()
                 
                 num_ue_fail_sum = 0
